3b7c36d3879979e35b405c9d17ffab24ea94ea39
Kaz <takaik@yahoo.com>
2022-04-12 13:32:57 -0400

    install axios and create login route

---
+++ src/views/LoginView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login container">
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        user: {
+          email: "",
+          password: "",
+        },
+      }
+    },
+    created() {
+
+    },
+    methods: {
+
+    },
+  }
+</script>
+
+<style>
+</style>
